a6cf22f8e4e8d9a57432242b2639d36ae5f8fc52
Sergey <serg1999g@gmail.com>
2019-12-19 14:41:40 +0200

    made the main blade.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,20 +1,20 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
+@extends('layout')
 
-<form action="{{ route('store') }}" method="post">
-    {{ csrf_field() }}
-    <label for="ip">Enter ip address</label>
-    <input type="text" name="ip">
-    <button>Get location</button>
-</form>
 
-</body>
-</html>
+@section('content')
+
+    <section>
+        <div class="container">
+            <div class="row">
+                <form action="{{ route('store') }}" method="post">
+                    {{ csrf_field() }}
+                    <label for="ip">Enter ip address</label>
+                    <input type="text" name="ip">
+                    <button>Get location</button>
+                </form>
+            </div>
+        </div>
+    </section>
+
+@endsection
+
--- resources/views/store.blade.php
+++ resources/views/store.blade.php
@@ -1,33 +1,30 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <link rel="stylesheet" href="{{asset('css/app.css')}}">
-</head>
-<body>
+@extends('layout')
 
-<div id="app">
-    @foreach($city as $item)
-        <div>
-            latitude: {{$item['latitude']}}
-        </div>
-        <div>
-            longitude: {{$item['longitude']}}
-        </div>
-        <div>
-            time zone: {{$item['time_zone']}}
-        </div>
-    @endforeach
 
-    <div>
-        <geo-location :urldata="{{json_encode($city)}}"></geo-location>
-    </div>
+@section('content')
 
-</div>
-<script src="{{asset('js/app.js')}}"></script>
-</body>
-</html>
+    <section>
+        <div class="container">
+            <div class="row">
+                @foreach($city as $item)
+                    <div>
+                        <div>
+                            latitude: {{$item['latitude']}}
+                        </div>
+                        <div>
+                            longitude: {{$item['longitude']}}
+                        </div>
+                        <div>
+                            time zone: {{$item['time_zone']}}
+                        </div>
+                    </div>
+                @endforeach
+
+                <div>
+                    <geo-location :urldata="{{json_encode($city)}}"></geo-location>
+                </div>
+            </div>
+        </div>
+    </section>
+
+@endsection
